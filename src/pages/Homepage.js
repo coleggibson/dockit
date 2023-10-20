6c2e90fcf9c440787530a808300181d0f472ce69
Cole Gibson <coleggibson@gmail.com>
2023-10-19 21:41:19 -0500

    Created React Project

---
+++ src/pages/Homepage.js
@@ -0,0 +1,12 @@
+import TextEditor from '../TextEditor';
+
+function Homepage () {
+    return (
+        <div>
+        <h1>Homepage shiii</h1>
+        <TextEditor/>
+        </div>
+    )
+}
+
+export default Homepage
