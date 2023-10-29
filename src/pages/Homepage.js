3db0c54de3e47586becab758608784ba49d22245
Cole Gibson <coleggibson@gmail.com>
2023-10-29 16:33:35 -0500

    Created space for HTML version of document to be stored

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,10 +1,9 @@
-import TextEditor from '../TextEditor';
 
 function Homepage () {
     return (
-        <div>
-        <h1>Homepage shiii</h1>
-        <TextEditor/>
+        <div class="bg-gray-100">
+            <h1 class="font-bold">Homepage shiii</h1>
+            <div id="pdf" class="h-[1056px] w-[816px] border-2 border-black">This is my document that we are downloading</div>
         </div>
     )
 }
